f22329e54a1d7aaea5c0374d32a2d503bfdd4c0e
Anton <golvcenkoanton@gmail.com>
2024-11-01 15:18:50 +0100

    Change test name to DepositHandler and remove default code

---
+++ test/DepositHandler.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DepositHandler} from "../src/DepositHandler.sol";
+
+contract DepositHandlerTest is Test {
+    DepositHandler public depositHandler;
+
+    function setUp() public {
+        depositHandler = new DepositHandler();
+    }
+}
