0ef426dda1146151244d561effe6c67b059d709b
Anton <golvcenkoanton@gmail.com>
2024-11-01 15:18:15 +0100

    Change script name to DepositHandler and remove default code

---
+++ script/DepositHandler.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DepositHandler} from "../src/DepositHandler.sol";
+
+contract DepositHandlerScript is Script {
+    DepositHandler public depositHandler;
+
+    function setUp() public {}
+
+    function run() public {}
+}
