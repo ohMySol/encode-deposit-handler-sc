7be1078f43f2aa7faa7f25b461455107b8e40f38
ok567 <harrison.stoddart995@gmail.com>
2024-11-01 15:53:57 +0000

    started depositHandler contract

--- src/DepositHandler.sol
+++ src/DepositHandler.sol
@@ -1,5 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract DepositHandler {
+    address public admin;
+    IERC20 public usdcToken;
+    mapping(address => uint256) public deposits; 
+    mapping(address => bool) public bootcampCompleted;
+
+    constructor(address _usdcToken) {
+        admin = msg.sender;
+        usdcToken = IERC20(_usdcToken);
+    }
+
+    // Allow users to deposit USDC
+    function deposit(uint256 amount) external {
+        require(amount >= 249990000 && amount =< 250010000, "Deposit must be 250USDC"); //we can change this if the deposit is different for different bootcamps
+        require(usdcToken.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        deposits[msg.sender] += amount;
+    }
+
+    // Mark a user as having completed the bootcamp (only admin)
+    function markBootcampComplete(address user) external {
+        require(msg.sender == admin, "Only admin can mark completion");
+        bootcampCompleted[user] = true;
+    }
+
+    // Withdraw funds if bootcamp is completed
+    function withdraw() external {
+        require(bootcampCompleted[msg.sender], "Bootcamp not successfully completed");
+        uint256 depositAmount = deposits[msg.sender];
+        require(depositAmount > 0, "No deposit to withdraw");
+
+        deposits[msg.sender] = 0;  //prevent re-entrancy
+        require(usdcToken.transfer(msg.sender, depositAmount), "Transfer failed");
+    }
 }
