be4d76c265afc7d9f3ffc48949bb3205f214dd5e
Anton <golvcenkoanton@gmail.com>
2024-11-03 18:18:19 +0100

    Add 2 interfaces with custom errors for: DepositHandler, BootcampManager

---
+++ src/interfaces/ICustomErrors.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+/// Custom errors for DepositHandler.sol
+interface IDepositHandlerErrors {
+    
+}
+
+/// Custom errors for BootcampManager.sol
+interface IBootcampManagerErrors {
+    
+}
