fb128f50b02a532a6665224678d1b3a20d956328
Anton <golvcenkoanton@gmail.com>
2024-11-03 18:21:51 +0100

    Separate USER, ADMIN functions | change pragma back to 0.8.28 | add interface for custom err

--- src/DepositHandler.sol
+++ src/DepositHandler.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IDepositHandlerErrors} from "./interfaces/ICustomErrors.sol";
 
-contract DepositHandler {
+contract DepositHandler is IDepositHandlerErrors {
     address public admin;
     IERC20 public usdcToken;
     //mapping(address => uint256) public deposits; 
@@ -13,29 +14,29 @@ contract DepositHandler {
     struct depositInfo{
         uint256 depositedAmount;
         bool bootcampCompleted; //this is set by an admin from encode (Centralised)
-
         bool yeild; //have not set this up yet
         bool multiSender; //have not set this up yet
-
     }
-
 
     constructor(address _usdcToken) {
         admin = msg.sender;
         usdcToken = IERC20(_usdcToken);
     }
 
-    // Allow users to deposit USDC
-    function deposit(uint256 amount) external { 
-        require(amount >= 249e18 && amount <= 251e18, "Deposit must be 250USDC"); //maybe try and not hardcode, try and get the deposit value from the bootcampmanager 
-        require(usdcToken.transferFrom(msg.sender, address(this), amount), "Transfer failed");
-        userDepositInfo[msg.sender].depositedAmount += amount;
-    }
+    /*//////////////////////////////////////////////////
+                USER FUNCTIONS
+    /////////////////////////////////////////////////*/
 
-    // Mark a user as having completed the bootcamp (only admin)
-    function markBootcampComplete(address user) external {
-        require(msg.sender == admin, "Only admin can mark completion"); //this could be done with a modifier instead to save gas?
-        userDepositInfo[user].bootcampCompleted = true;
+    /**
+     * @dev Allow user to deposit USDC '_amount' for a specific bootcamp. 
+     * Deposited amount will be locked inside this contract till the end of
+     * the bootcamp.
+     * @param _amount - USDC amount,
+     */
+    function deposit(uint256 _amount) external { 
+        require(_amount >= 249e18 && _amount <= 251e18, "Deposit must be 250USDC"); //maybe try and not hardcode, try and get the deposit value from the bootcampmanager +
+        require(usdcToken.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+        userDepositInfo[msg.sender].depositedAmount += _amount;
     }
 
     // Withdraw funds if bootcamp is completed
@@ -50,5 +51,15 @@ contract DepositHandler {
         userInfo.depositedAmount = 0;
 
         require(usdcToken.transfer(msg.sender, depositAmount), "Transfer failed"); //change this to .call{value:....} as this the most approved way of transferring funds
+    }
+
+    /*//////////////////////////////////////////////////
+                ADMIN FUNCTIONS
+    /////////////////////////////////////////////////*/
+
+    // Mark a user as having completed the bootcamp (only admin)
+    function markBootcampComplete(address user) external {
+        require(msg.sender == admin, "Only admin can mark completion"); //this could be done with a modifier instead to save gas?
+        userDepositInfo[user].bootcampCompleted = true;
     }
 }
