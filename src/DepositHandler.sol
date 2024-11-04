2ebc084e0d312cbd3c946c73623037b3dadc7aaf
Anton <golvcenkoanton@gmail.com>
2024-11-04 16:25:14 +0100

    Add pausable mechanism | add ownable mecahnism

--- src/DepositHandler.sol
+++ src/DepositHandler.sol
@@ -13,7 +13,7 @@ import {IDepositHandlerErrors} from "./interfaces/ICustomErrors.sol";
  * Implements both user part and admin part for deposit management.
  * Apart from that contract allow to manage admins and bootcamps.
  */
-contract DepositHandler is IDepositHandlerErrors{
+contract DepositHandler is IDepositHandlerErrors, Pausable, Ownable {
     address public admin;
     IERC20 public usdcToken;
     //mapping(address => uint256) public deposits; 
@@ -27,7 +27,7 @@ contract DepositHandler is IDepositHandlerErrors{
         bool multiSender; //have not set this up yet
     }
 
-    constructor(address _usdcToken) {
+    constructor(address _usdcToken) Ownable(msg.sender) {
         admin = msg.sender;
         usdcToken = IERC20(_usdcToken);
     }
@@ -41,7 +41,7 @@ contract DepositHandler is IDepositHandlerErrors{
      * the bootcamp.
      * @param _amount - USDC amount.
      */
-    function deposit(uint256 _amount) external { 
+    function deposit(uint256 _amount) external whenNotPaused { 
         require(_amount >= 249e18 && _amount <= 251e18, "Deposit must be 250USDC"); //maybe try and not hardcode, try and get the deposit value from the bootcampmanager +
         require(usdcToken.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
         userDepositInfo[msg.sender].depositedAmount += _amount;
@@ -52,7 +52,7 @@ contract DepositHandler is IDepositHandlerErrors{
      * Deposited amount will be locked inside this contract till the end of
      * the bootcamp.
      */
-    function withdraw() external {
+    function withdraw() external whenNotPaused {
         depositInfo storage userInfo = userDepositInfo[msg.sender];
 
         require(userInfo.bootcampCompleted, "Bootcamp not successfully completed");
@@ -72,5 +72,24 @@ contract DepositHandler is IDepositHandlerErrors{
     function markBootcampComplete(address user) external {
         require(msg.sender == admin, "Only admin can mark completion"); //this could be done with a modifier instead to save gas?
         userDepositInfo[user].bootcampCompleted = true;
+    }
+
+    /*//////////////////////////////////////////////////
+                OWNER FUNCTIONS
+    /////////////////////////////////////////////////*/
+    /**
+     * @dev Contract owner is able to put a contract on pause in case of vulnerability
+     * or any other problem. Functions using 'whenNotPaused()' modifier won't work.
+     */
+    function pause() private onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev Contract owner is able to unpause a contract when vulnerability or
+     * any other problem is resolved. Functions using 'whenNotPaused()' modifier will work.
+     */
+    function unpause() private onlyOwner {
+        _unpause();
     }
 }
