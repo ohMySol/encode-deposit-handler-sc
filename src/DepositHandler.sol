dc862c62b92e330dab406a1ff0bbb390f151390e
Anton <golvcenkoanton@gmail.com>
2024-11-05 21:40:01 +0100

    Upd constructor, storage variables, functions

--- src/DepositHandler.sol
+++ src/DepositHandler.sol
@@ -13,22 +13,22 @@ import {IDepositHandlerErrors} from "./interfaces/ICustomErrors.sol";
  * Apart from that contract allow to manage admins and bootcamps.
  */
 contract DepositHandler is IDepositHandlerErrors, Pausable {
-    IERC20 public immutable usdc; //at the moment assume that Encode using only USDC.
-    address public admin;
-    struct depositInfo{
+    uint256 public immutable depositAmount;
+    IERC20 public immutable depositToken;
+    struct depositInfo {
         uint256 depositedAmount;
         bool bootcampCompleted; //this is set by an admin from encode (Centralised)
         bool yeild; //have not set this up yet
         bool multiSender; //have not set this up yet
     }
-
+    
     //mapping(address => uint256) public deposits; 
     //mapping(address => bool) public bootcampCompleted;
     mapping(address => depositInfo) public userDepositInfo;
     
-    constructor(address _usdcToken) {
-        usdc = IERC20(_usdcToken);
-        admin = msg.sender;
+    constructor(uint256 _depositAmount, IERC20 _depositToken) {
+        depositAmount = _depositAmount;
+        depositToken = _depositToken;
     }
 
     /*//////////////////////////////////////////////////
@@ -42,7 +42,7 @@ contract DepositHandler is IDepositHandlerErrors, Pausable {
      */
     function deposit(uint256 _amount) external whenNotPaused { 
         require(_amount >= 249e18 && _amount <= 251e18, "Deposit must be 250USDC"); //maybe try and not hardcode, try and get the deposit value from the bootcampmanager +
-        require(usdc.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+        require(depositToken.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
         userDepositInfo[msg.sender].depositedAmount += _amount;
     }
 
@@ -61,17 +61,18 @@ contract DepositHandler is IDepositHandlerErrors, Pausable {
         // Reset deposit before transferring to prevent re-entrancy
         userInfo.depositedAmount = 0;
 
-        require(usdc.transfer(msg.sender, depositAmount), "Transfer failed"); //change this to .call{value:....} as this the most approved way of transferring funds +
+        require(depositToken.transfer(msg.sender, depositAmount), "Transfer failed"); //change this to .call{value:....} as this the most approved way of transferring funds +
     }
 
     /*//////////////////////////////////////////////////
                 ADMIN FUNCTIONS
     /////////////////////////////////////////////////*/
     // Mark a user as having completed the bootcamp (only admin)
-    function markBootcampComplete(address user) external {
+    /* function markBootcampComplete(address user) external {
+        // ! Require access control mechanism with the roles like in BootcampDepositFactory.sol.
         require(msg.sender == admin, "Only admin can mark completion"); //this could be done with a modifier instead to save gas?
         userDepositInfo[user].bootcampCompleted = true;
-    }
+    } */
 
     /*//////////////////////////////////////////////////
                 OWNER FUNCTIONS
