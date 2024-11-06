d2464d35039843b8dd1d234a444c476c6429307d
Anton <golvcenkoanton@gmail.com>
2024-11-06 14:59:07 +0100

    Add description to the contract, functions | create a struct for Bootcamp

--- src/BootcampFactory.sol
+++ src/BootcampFactory.sol
@@ -5,24 +5,53 @@ import "./DepositHandler.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import {IBootcampFactoryErrors} from "./interfaces/ICustomErrors.sol";
 
+/// @title Bootcamp Factory contract.
+/// @author @ohMySol, @nynko, @ok567
+/// @notice Contract create new bootcamps.
+/// @dev Contract for creation new instances of the `DepositHandler` contract with a Factory pattern. 
+/// New instances are stored inside this factory contract and they can be quickly retrieved for the 
+/// information and frontend usage. 
 contract BootcampFactory is AccessControl, IBootcampFactoryErrors {
+    bytes32 public constant ADMIN = keccak256("ADMIN"); // Main Role
+    bytes32 public constant MANAGER = keccak256("MANAGER"); // 2nd Roles
+   
+    uint256 public totalBootcampAmount;
+    mapping (uint256 => Bootcamp) public bootcamps;
+
     event BootcampCreated (
         uint256 bootcampId,
         uint256 depositAmount,
+        address depositToken,
         address bootcampAddress
     );
-
-    bytes32 public constant ADMIN = keccak256("ADMIN"); // Main Role
-    bytes32 public constant MANAGER = keccak256("MANAGER"); // 2nd Roles
-    uint256 public totalBootcampAmount;
-
-    mapping (uint256 => address) public bootcamps;
-
+    struct Bootcamp {
+        uint256 id;
+        uint256 depositAmount;
+        address depositToken;
+        address bootcampAddress;
+    }
+    
     constructor() {
         _grantRole(ADMIN, msg.sender); // Grant the deployer the admin role
         _setRoleAdmin(MANAGER, ADMIN); // Set the `ADMIN` role as the administrator for the `MANAGER` role
     }
 
+    /*//////////////////////////////////////////////////
+                MANAGER FUNCTIONS
+    /////////////////////////////////////////////////*/
+    /**
+     * @notice Managers are able to create a new bootcamp instance each time
+     * they need to launch a new bootcamp.
+     * @dev Create a new `DepositHandler` contract instance and set up a required 
+     * bootcamp information in this instance: `_depositAmount` and `_depositToken`.
+     * New bootcamp instance is stored in this factory contract in `bootcamp` mapping 
+     * by unique id.
+     * 
+     * Emits a {BootcampCreated} event.
+     * .
+     * @param _depositAmount - bootcamp deposit amount.
+     * @param _depositToken  - token address which is used for deposit. 
+     */
     function createBootcamp(uint256 _depositAmount, address _depositToken) external onlyRole(MANAGER) {
         if (_depositToken == address(0)) {
             revert BootcampFactory__DepositTokenCanNotBeZeroAddress();
@@ -30,11 +59,24 @@ contract BootcampFactory is AccessControl, IBootcampFactoryErrors {
         DepositHandler bootcamp = new DepositHandler(_depositAmount, _depositToken);
 
         totalBootcampAmount++;
-        bootcamps[totalBootcampAmount] = address(bootcamp);
+        bootcamps[totalBootcampAmount] = Bootcamp({
+            id: totalBootcampAmount,
+            depositAmount: _depositAmount,
+            depositToken: _depositToken,
+            bootcampAddress: address(bootcamp)
+        });
         
-        emit BootcampCreated(totalBootcampAmount, _depositAmount, address(bootcamp));
+        emit BootcampCreated(totalBootcampAmount, _depositAmount, _depositToken, address(bootcamp));
     }
 
+    /*//////////////////////////////////////////////////
+                ADMIN FUNCTIONS
+    /////////////////////////////////////////////////*/
+    /**
+     * @dev Set `MANAGER` role to `_manager` address.
+     * 
+     * @param _manager - address of the user that will have a `MANAGER` role.
+     */
     function grantManagerRole(address _manager) external onlyRole(ADMIN) {
         if (_manager == address(0)) {
             revert BootcampFactory__CanNotGrantRoleToZeroAddress();
@@ -42,6 +84,11 @@ contract BootcampFactory is AccessControl, IBootcampFactoryErrors {
         _grantRole(MANAGER, _manager);
     }
 
+    /**
+     * @dev Remove `MANAGER` role from `_manager` address.
+     * 
+     * @param _manager - address of the user that has a `MANAGER` role.
+     */
     function revokeManagerRole(address _manager) external onlyRole(ADMIN) {
         if (_manager == address(0)) {
             revert BootcampFactory__CanNotGrantRoleToZeroAddress();
