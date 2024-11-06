1db2e11423bace7e635f01f0634b6dc6760ca5ad
Anton <golvcenkoanton@gmail.com>
2024-11-06 14:19:51 +0100

    Add custom errors in ICustomErrors.sol for BootcampFactory.sol

--- src/interfaces/ICustomErrors.sol
+++ src/interfaces/ICustomErrors.sol
@@ -5,3 +5,17 @@ pragma solidity 0.8.28;
 interface IDepositHandlerErrors {
     
 }
+
+/// Custom errors for BootcampFactory.sol
+interface IBootcampFactoryErrors {
+    /**
+     * @dev Error indicates that user tries to grant a role to `addres(0)`.
+     */
+    error BootcampFactory__CanNotGrantRoleToZeroAddress();
+
+    /**
+     * @dev Error indicates that user tries to create a new bootcamp instance
+     * with token address = `addres(0)`.
+     */
+    error BootcampFactory__DepositTokenCanNotBeZeroAddress();
+}
