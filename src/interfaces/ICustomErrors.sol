77964fafd04b44676d30356001e47c41189403ed
Anton <golvcenkoanton@gmail.com>
2024-11-07 16:34:55 +0100

    Add custom error for IBootcampFactoryErrors

--- src/interfaces/ICustomErrors.sol
+++ src/interfaces/ICustomErrors.sol
@@ -14,6 +14,11 @@ interface IBootcampFactoryErrors {
     error BootcampFactory__CanNotGrantRoleToZeroAddress();
 
     /**
+     * @dev Error indicates that user tries to revoke a role from `addres(0)`.
+     */
+    error BootcampFactory__CanNotRevokeRoleFromZeroAddress();
+
+    /**
      * @dev Error indicates that user tries to create a new bootcamp instance
      * with token address = `addres(0)`.
      */
