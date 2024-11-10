b485159058b7891976ab2120defe96fa6f2c5fee
Anton <golvcenkoanton@gmail.com>
2024-11-10 15:49:13 +0100

    Add statuses to control user depositing/withdrawing + add manager functions

--- src/DepositHandler.sol
+++ src/DepositHandler.sol
@@ -25,13 +25,17 @@ contract DepositHandler is Pausable, AccessControl, IDepositHandlerErrors {
     uint256 public immutable depositAmount;
     uint256 public immutable bootcampDuration;
     uint256 public immutable bootcampStartTime;
-    IERC20 public immutable depositToken;
-    //mapping(address => bool) public bootcampCompleted;
+    IERC20 public immutable depositToken;    
     mapping(address => depositInfo) public deposits;
 
+    enum Status { // status of the bootcamp participant. 
+        Deposit,
+        InProgress,
+        Withdraw
+    }
     struct depositInfo {
         uint256 depositedAmount;
-        bool bootcampCompleted; //this is set by an admin from encode (Centralised)
+        Status status; //this is set by an admin from encode
     }
     event DepositDone(
         address depositor,
@@ -41,6 +45,14 @@ contract DepositHandler is Pausable, AccessControl, IDepositHandlerErrors {
         address depositor,
         uint256 withdrawAmount
     );
+
+    modifier isAllowed(Status _status) {
+        Status status = deposits[msg.sender].status;
+        if (status != _status) {
+            revert DepositHandler__NotAllowedActionWithYourStatus();
+        }
+        _;
+    }
 
     constructor(
         uint256 _depositAmount, 
@@ -62,14 +74,17 @@ contract DepositHandler is Pausable, AccessControl, IDepositHandlerErrors {
      * @notice Do deposit of USDC into this contract.
      * @dev Allow `_depositor` to deposit USDC '_amount' for a bootcamp. 
      * Deposited amount will be locked inside this contract till the end of
-     * the bootcamp. Function restricted to be called only when contract is not on Pause.
+     * the bootcamp. 
+     * Function restrictions: 
+     *  - Contract shouldn't be on Pause.
+     *  - Can only be called when user has a status `Deposit`.
      * 
      * Emits a {DepositDone} event.
      * 
      * @param _amount - USDC amount.
      * @param _depositor - address of the bootcamp participant.
      */
-    function deposit(uint256 _amount, address _depositor) external whenNotPaused { 
+    function deposit(uint256 _amount, address _depositor) external whenNotPaused isAllowed(Status.Deposit) { 
         uint256 allowance = depositToken.allowance(_depositor, address(this));
         if (_amount != depositAmount) {
             revert DepositHandler__IncorrectDepositedAmount(_amount);
@@ -85,14 +100,16 @@ contract DepositHandler is Pausable, AccessControl, IDepositHandlerErrors {
 
     /**
      * @dev Allow `_depositor` to withdraw USDC '_amount' from a bootcamp. 
-     * Function restricted to be called only when contract is not on Pause.
+     * Function restrictions: 
+     *  - Contract shouldn't be on Pause.
+     *  - Can only be called when user has a status `Withdraw`.
      * 
      * Emits a {DepositWithdrawn} event.
      * 
      * @param _amount - USDC amount.
      * @param _depositor - address of the bootcamp participant.
      */
-    function withdraw(uint256 _amount, address _depositor) external whenNotPaused {
+    function withdraw(uint256 _amount, address _depositor) external whenNotPaused isAllowed(Status.Withdraw) {
         if (deposits[_depositor].depositedAmount != _amount) {
             revert DepositHandler__IncorrectAmountForWithdrawal(_amount);
         }
@@ -116,7 +133,8 @@ contract DepositHandler is Pausable, AccessControl, IDepositHandlerErrors {
      * @notice Set contract on pause, and stop interraction with critical functions.
      * @dev Manager is able to put a contract on pause in case of vulnerability
      * or any other problem. Functions using `whenNotPaused()` modifier won't work.
-     * Function restricted to be called only by `MANAGER` of this contract.
+     * Function restrictions:
+     *  - Can only be called by `MANAGER` of this contract.
      */
     function pause() private onlyRole(MANAGER) {
         _pause();
@@ -126,9 +144,76 @@ contract DepositHandler is Pausable, AccessControl, IDepositHandlerErrors {
      * @notice Remove contract from pause, and allow interraction with critical functions.
      * @dev Manager is able to unpause a contract when vulnerability or
      * any other problem is resolved. Functions using `whenNotPaused()` modifier will work.
-     * Function restricted to be called only by `MANAGER` of this contract.
+     * Function restrictions:
+     *  - Can only be called by `MANAGER` of this contract.
      */
     function unpause() private onlyRole(MANAGER) {
         _unpause();
+    }
+
+    /**
+     * @dev Set `Deposit` status for all addresses in the `_participants` array.
+     * Faster way to set status for a a list of participants instead of calling on eby one.
+     * Function restrictions:
+     *  - Can only be called by `MANAGER` of this contract.
+     * 
+     * @param _participants - array of participants addresses.
+     */
+    function allowDepositBatch(address[] calldata _participants) external onlyRole(MANAGER) {
+        uint256 length = _participants.length;
+        if (length == 0) {
+            revert DepositHandler__ParticipantsArraySizeIsZero();
+        }
+        for (uint i = 0; i < length; i++) {
+            _allowDeposit(_participants[i]);
+        }
+    }
+
+    /**
+     * @dev Set `Withdraw` status for all addresses in the `_participants` array.
+     * Faster way to set status for a a list of participants instead of calling on eby one.
+     * Function restrictions:
+     *  - Can only be called by `MANAGER` of this contract.
+     * 
+     * @param _participants - array of participants addresses.
+     */
+    function allowWithdrawBatch(address[] calldata _participants) external onlyRole(MANAGER) {
+        uint256 length = _participants.length;
+        if (length == 0) {
+            revert DepositHandler__ParticipantsArraySizeIsZero();
+        }
+        for (uint i = 0; i < length; i++) {
+            _allowWithdraw(_participants[i]);
+        }
+    }
+    
+    /**
+     * @dev Set `Deposit` status for the `_participant` address, so that `_participant` will be able
+     * to do a deposit.
+     * Function restrictions:
+     *  - Can only be called by `MANAGER` of this contract.
+     * 
+     * @param _participant - address of the user.
+     */
+    function _allowDeposit(address _participant) internal onlyRole(MANAGER) {
+        if (_participant == address(0)) {
+            revert DepositHandler__ParticipantAddressZero();
+        }
+        deposits[_participant].status = Status.Deposit;
+    }
+
+    /**
+     * @dev Set `Withdraw` status for the `_participant` address, so that `_participant` will be able
+     * to do a withdraw a deposit.
+     * Function restrictions:
+     *  - Can only be called by `MANAGER` of this contract.
+     * 
+     * @param _participant - address of the user.
+     */
+    function _allowWithdraw(address _participant) internal onlyRole(MANAGER) {
+        if (_participant == address(0)) {
+            revert DepositHandler__ParticipantAddressZero();
+        }
+        deposits[_participant].status = Status.Withdraw;
     }
 }
