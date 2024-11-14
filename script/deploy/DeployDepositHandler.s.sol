bd48c55c590efc21ac4d81901568e15368a0b811
Anton <golvcenkoanton@gmail.com>
2024-11-14 20:14:26 +0100

    Add script for Deposithandler deployment

---
+++ script/deploy/DeployDepositHandler.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {Script, console} from "forge-std/Script.sol";
+
+contract DeployDepositHandler is Script{
+    constructor() {
+        
+    }
+}
