dd765c6ebc2377ec93fd357a6375d5ff0ffef57f
Anton <golvcenkoanton@gmail.com>
2024-11-17 02:45:16 +0100

    Finished tests for BootcampFactory contract

--- test/BootcampFactory.t.sol
+++ test/BootcampFactory.t.sol
@@ -21,6 +21,18 @@ contract BootcampFactoryTest is Test {
     address public manager;
     address public alice;
 
+    event BootcampCreated (
+        uint256 indexed bootcampId,
+        address indexed bootcampAddress
+    );
+
+    // beforeEach hook
+    modifier adminGrantManagerRole() {
+        vm.startPrank(admin);
+        factory.grantRole(MANAGER, manager);
+        _;
+    }
+
     function setUp() public {
         DeployBootcampFactoryScript deployer = new DeployBootcampFactoryScript();
         (factory, helperConfig) = deployer.deploy();
@@ -44,18 +56,18 @@ contract BootcampFactoryTest is Test {
 
 
     /*//////////////////////////////////////////////////
-                GRANTAROLE TESTS
+                GRANTROLE TESTS
     /////////////////////////////////////////////////*/
     function test_AdminSuccessfullyGrantManagerRoleToUser() public {
         vm.prank(admin);
-        factory.grantARole(MANAGER, manager);
+        factory.grantRole(MANAGER, manager);
         
         assertTrue(factory.hasRole(MANAGER, manager));
     }
 
     function test_AdminSuccessfullyGrantAdminRoleToUser() public {
         vm.prank(admin);
-        factory.grantARole(ADMIN, alice);
+        factory.grantRole(ADMIN, alice);
         
         assertTrue(factory.hasRole(ADMIN, alice));
     }
@@ -68,7 +80,7 @@ contract BootcampFactoryTest is Test {
             ADMIN
         ));
 
-        factory.grantARole(MANAGER, manager);
+        factory.grantRole(MANAGER, manager);
     }
 
     function test_AccountReceivingNewRoleCanNotBeAddressZero() public {
@@ -77,7 +89,7 @@ contract BootcampFactoryTest is Test {
             IBootcampFactoryErrors.BootcampFactory__CanNotUpdateRoleForZeroAddress.selector
         );
 
-        factory.grantARole(MANAGER, address(0));
+        factory.grantRole(MANAGER, address(0));
     }
 
     function test_AdminCanNotGrantNonExistentRole() public {
@@ -87,65 +99,52 @@ contract BootcampFactoryTest is Test {
             INVALID_ROLE
         ));
 
-        factory.grantARole(INVALID_ROLE, manager);
+        factory.grantRole(INVALID_ROLE, manager);
     }
 
 
 
     /*//////////////////////////////////////////////////
-                REVOKEAROLE TESTS
+                REVOKEROLE TESTS
     /////////////////////////////////////////////////*/
-    function test_AdminSuccessfullyRevokeManagerRoleFromUser() public {
-        vm.startPrank(admin);
-        factory.grantARole(MANAGER, manager);
-        factory.revokeARole(MANAGER, manager);
+    function test_AdminSuccessfullyRevokeManagerRoleFromUser() public adminGrantManagerRole {
+        factory.revokeRole(MANAGER, manager);
         
         assertFalse(factory.hasRole(MANAGER, manager));
     }
     
-     function test_AdminSuccessfullyRevokeAdminRoleFromUser() public {
-        vm.startPrank(admin);
-        factory.grantARole(ADMIN, alice);
-        factory.revokeARole(ADMIN, alice);
+     function test_AdminSuccessfullyRevokeAdminRoleFromUser() public adminGrantManagerRole {
+        factory.revokeRole(ADMIN, alice);
 
         assertFalse(factory.hasRole(ADMIN, alice));
     }
 
-    function test_OnlyAdminCanRevokeTheRoles() public {
-        vm.prank(admin);
-        factory.grantARole(MANAGER, manager);
-        
-        vm.prank(alice);
+    function test_OnlyAdminCanRevokeTheRoles() public adminGrantManagerRole {
+        vm.startPrank(alice);
         vm.expectRevert(abi.encodeWithSelector(
             IAccessControl.AccessControlUnauthorizedAccount.selector,
             alice,
             ADMIN
         ));
 
-        factory.revokeARole(MANAGER, manager);
+        factory.revokeRole(MANAGER, manager);
     }
 
-    function test_AccountLosingARoleCanNotBeAddressZero() public {
-        vm.startPrank(admin);
-        factory.grantARole(MANAGER, manager);
-
+    function test_AccountLosingARoleCanNotBeAddressZero() public adminGrantManagerRole {
         vm.expectRevert(
             IBootcampFactoryErrors.BootcampFactory__CanNotUpdateRoleForZeroAddress.selector
         );
 
-        factory.revokeARole(MANAGER, address(0));
+        factory.revokeRole(MANAGER, address(0));
     }
     
-    function test_AdminCanNotRevokeNonExistentRole() public {
-        vm.startPrank(admin);
-        factory.grantARole(MANAGER, manager);
-
+    function test_AdminCanNotRevokeNonExistentRole() public adminGrantManagerRole {
         vm.expectRevert(abi.encodeWithSelector(
             IBootcampFactoryErrors.BootcampFactory__UpdateNonExistentRole.selector, 
             INVALID_ROLE
         ));
 
-        factory.revokeARole(INVALID_ROLE, manager);
+        factory.revokeRole(INVALID_ROLE, manager);
     }
 
 
@@ -153,18 +152,84 @@ contract BootcampFactoryTest is Test {
     /*//////////////////////////////////////////////////
                 CREATEBOOTCAMP TESTS
     /////////////////////////////////////////////////*/
-    function test_ManagerSuccessfullyCreateNewBootcamp() public {
-        vm.prank(admin);
-        factory.grantARole(MANAGER, manager);
-
-        vm.prank(manager);
+    function test_ManagerSuccessfullyCreateNewBootcamp() public adminGrantManagerRole {
+        vm.startPrank(manager);
         factory.createBootcamp(
             networkConfig.depositAmount,
             networkConfig.depositToken,
             networkConfig.bootcampStartTime
         );
         uint256 bootcampId = factory.totalBootcampAmount();
-        console.log(bootcampId);
-        //BootcampFactory.Bootcamp memory bootcamp = factory.bootcamps(bootcampId);
+        (
+            uint256 id, 
+            uint256 depositAmount, 
+            address depositToken, 
+            address bootcampAddress
+        ) = factory.bootcamps(bootcampId);
+        
+        assertEq(id, 1);
+        assertEq(depositAmount, networkConfig.depositAmount);
+        assertEq(depositToken, networkConfig.depositToken);
+        assertTrue(bootcampAddress != address(0));
+    }
+
+    function test_EventIsEmittedOnceBootcampIsCreated() public adminGrantManagerRole {
+        vm.startPrank(manager);
+        // recording data from events
+        vm.recordLogs();
+        factory.createBootcamp(
+            networkConfig.depositAmount,
+            networkConfig.depositToken,
+            networkConfig.bootcampStartTime
+        );
+        Vm.Log[] memory logs = vm.getRecordedLogs(); // receive all the recorded logs
+        uint256 bootcampId = uint256(logs[1].topics[1]);
+        address bootcampAddress = address(uint160(uint256(logs[1].topics[2])));
+        
+        assertEq(bootcampId, 1);
+        assertEq(bootcampAddress, 0xa16E02E87b7454126E5E10d957A927A7F5B5d2be);
+    }
+
+    function test_OnlyManagerCanCreateNewBootcamp() public adminGrantManagerRole {
+        vm.startPrank(alice);
+        vm.expectRevert(abi.encodeWithSelector(
+            IAccessControl.AccessControlUnauthorizedAccount.selector,
+            alice,
+            MANAGER
+        ));
+
+        factory.createBootcamp(
+            networkConfig.depositAmount,
+            networkConfig.depositToken,
+            networkConfig.bootcampStartTime
+        );
+    }
+
+    function test_DepositTokenForBootcampCanNotBeAddressZero() public adminGrantManagerRole {
+        vm.startPrank(manager);
+        vm.expectRevert(
+            IBootcampFactoryErrors.BootcampFactory__DepositTokenCanNotBeZeroAddress.selector
+        );
+
+        factory.createBootcamp(
+            networkConfig.depositAmount,
+            address(0),
+            networkConfig.bootcampStartTime
+        );
+    }
+
+    function test_BootcampStartDateCanNotBeInThePast() public adminGrantManagerRole {
+        vm.startPrank(manager);
+        vm.expectRevert(
+            IBootcampFactoryErrors.BootcampFactory__InvalidBootcampStartTime.selector
+        );
+
+        uint256 oldTimestamp = block.timestamp; // Fixed timestamp in the past
+        vm.warp(oldTimestamp + 1);
+        factory.createBootcamp(
+            networkConfig.depositAmount,
+            networkConfig.depositToken,
+            oldTimestamp
+        );
     }
 }
