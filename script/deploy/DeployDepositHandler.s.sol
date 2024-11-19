a90b82ce7b5fd5706ab57ec1f4088b924e4962cc
Anton <golvcenkoanton@gmail.com>
2024-11-19 18:24:25 +0100

    Update DeployDepositHandlerScript

--- script/deploy/DeployDepositHandler.s.sol
+++ script/deploy/DeployDepositHandler.s.sol
@@ -20,7 +20,11 @@ contract DeployDepositHandlerScript is Script{
             config.depositAmount,
             config.depositToken,
             vm.addr(config.manager),
-            config.bootcampStartTime
+            config.bootcampStart,
+            config.bootcampDeadline,
+            config.withdrawDuration,
+            address(0),
+            config.bootcampName
         );
         vm.stopBroadcast();
 
