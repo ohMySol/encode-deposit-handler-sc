acee644c7fb0d0fde2da8aa5f8adfe00ecb08947
Anton <golvcenkoanton@gmail.com>
2024-11-19 18:23:53 +0100

    Update config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.28;
 
 import {Script} from "forge-std/Script.sol";
 import {DepositTokenMock} from "../test/mocks/DepositTokenMock.sol";
+import {BootcampFactory} from "../src/BootcampFactory.sol";
 import {IHelperConfigErrors} from "../src/interfaces/ICustomErrors.sol";
 
 abstract contract Constants {
@@ -13,9 +14,13 @@ abstract contract Constants {
 
 contract HelperConfig is Script, Constants, IHelperConfigErrors {
     struct NetworkConfig {
-        address depositToken;
         uint256 depositAmount;
-        uint256 bootcampStartTime;
+        address depositToken;
+        uint256 bootcampStart;
+        uint256 bootcampDeadline;
+        uint256 withdrawDuration;
+        address factory;
+        string bootcampName;
         uint256 admin;
         uint256 manager;
     }
@@ -48,15 +53,21 @@ contract HelperConfig is Script, Constants, IHelperConfigErrors {
         if (localNetworkConfig.depositToken != address(0)) {
             return localNetworkConfig;
         } else {
-            // deploy mocks
+            uint256 _bootcampStart = block.timestamp + 10 minutes;
+            // deploy mocks and factory
             vm.startBroadcast();
             DepositTokenMock tokenMock = new DepositTokenMock();
+            BootcampFactory factory = new BootcampFactory();
             vm.stopBroadcast();
 
             localNetworkConfig = NetworkConfig({
-                depositToken: address(tokenMock),
                 depositAmount: 100,
-                bootcampStartTime: block.timestamp + 10 minutes,
+                depositToken: address(tokenMock),
+                bootcampStart: _bootcampStart,
+                bootcampDeadline: _bootcampStart + 10 minutes, // bootcamp dura
+                withdrawDuration: 10 minutes,
+                factory: address(factory),
+                bootcampName: "G5",
                 manager: vm.envUint("MANAGER_LOCAL_PK"), // pk from anvil list
                 admin: vm.envUint("ADMIN_LOCAL_PK") // pk from anvil list
             });
